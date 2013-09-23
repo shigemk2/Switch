f31d89d69fdcf17be53f6856b8c76fa477116e6e
Michihito Shigemura <i.am.shige@gmail.com>
2013-09-24 00:38:25 +0900

    Add magenta

--- app/switch_view_controller.rb
+++ app/switch_view_controller.rb
@@ -2,7 +2,7 @@
 class SwitchViewController < UIViewController
   COLORS = {
     'white' => UIColor.whiteColor, 'black' => UIColor.blackColor,
-    'red' => UIColor.redColor
+    'red' => UIColor.redColor, 'magenta' => UIColor.magentaColor,
   }
   def viewDidLoad
     super
@@ -21,6 +21,12 @@ class SwitchViewController < UIViewController
     @uiswitch_red.accessibilityLabel = 'switch black or red'
     @uiswitch_red.addTarget(self, action:'changeColorRed', forControlEvents:UIControlEventValueChanged)
     view.addSubview(@uiswitch_red)
+
+    @uiswitch_magenta = UISwitch.alloc.initWithFrame(CGRectMake(100, 180, 100, 100))
+    @uiswitch_magenta.onTintColor = UIColor.magentaColor
+    @uiswitch_magenta.accessibilityLabel = 'switch black or magenta'
+    @uiswitch_magenta.addTarget(self, action:'changeColorMagenta', forControlEvents:UIControlEventValueChanged)
+    view.addSubview(@uiswitch_magenta)
   end
 
   def changeColorWhite
@@ -30,7 +36,7 @@ class SwitchViewController < UIViewController
       view.backgroundColor    = COLORS['white']
       view.accessibilityLabel = 'white'
     else
-      view.backgroundColor = COLORS['black']
+      view.backgroundColor    = COLORS['black']
       view.accessibilityLabel = 'black'
     end
     self.checkSwitch
@@ -43,25 +49,40 @@ class SwitchViewController < UIViewController
       view.backgroundColor    = COLORS['red']
       view.accessibilityLabel = 'red'
     else
-      view.backgroundColor = COLORS['black']
+      view.backgroundColor    = COLORS['black']
+      view.accessibilityLabel = 'black'
+    end
+    self.checkSwitch
+  end
+
+  def changeColorMagenta
+    if @uiswitch_magenta.on?
+      self.disableAllSwitch
+      @uiswitch_magenta.enabled = true
+      view.backgroundColor      = COLORS['magenta']
+      view.accessibilityLabel   = 'magenta'
+    else
+      view.backgroundColor    = COLORS['black']
       view.accessibilityLabel = 'black'
     end
     self.checkSwitch
   end
 
   def checkSwitch
-    if not (@uiswitch_white.on? or @uiswitch_red.on?)
+    if not (@uiswitch_white.on? or @uiswitch_red.on? or @uiswitch_magenta.on?)
       self.enableAllSwitch
     end
   end
 
   def enableAllSwitch
-    @uiswitch_white.enabled = true
-    @uiswitch_red.enabled   = true
+    @uiswitch_white.enabled   = true
+    @uiswitch_red.enabled     = true
+    @uiswitch_magenta.enabled = true
   end
 
   def disableAllSwitch
-    @uiswitch_white.enabled = false
-    @uiswitch_red.enabled   = false
+    @uiswitch_white.enabled   = false
+    @uiswitch_red.enabled     = false
+    @uiswitch_magenta.enabled = false
   end
 end
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -27,9 +27,21 @@ describe "Application 'Switch'" do
     view("black").should.not.be.nil
   end
 
+  it "flick magenta switch once" do
+    flick 'switch black or magenta', :from => :left, :to => :right
+    view("magenta").should.not.be.nil
+  end
+
+  it "flick magenta switch twice" do
+    flick 'switch black or magenta', :from => :left, :to => :right
+    flick 'switch black or magenta', :from => :right, :to => :left
+    view("black").should.not.be.nil
+  end
+
   it "flick white but other color can't switch" do
     flick 'switch black or white', :from => :left, :to => :right
     flick 'switch black or red', :from => :left, :to => :right
+    flick 'switch black or magenta', :from => :left, :to => :right
 
     view("white").should.not.be.nil
   end
@@ -37,7 +49,16 @@ describe "Application 'Switch'" do
   it "flick red but other color can't switch" do
     flick 'switch black or red', :from => :left, :to => :right
     flick 'switch black or white', :from => :left, :to => :right
+    flick 'switch black or magenta', :from => :left, :to => :right
 
     view("red").should.not.be.nil
+  end
+
+  it "flick magenta but other color can't switch" do
+    flick 'switch black or magenta', :from => :left, :to => :right
+    flick 'switch black or red', :from => :left, :to => :right
+    flick 'switch black or white', :from => :left, :to => :right
+
+    view("magenta").should.not.be.nil
   end
 end
